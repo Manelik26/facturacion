ba4526af616e4a89c1e402239993acdc456637d0
Manelik26 <jhinojosa03@gmail.com>
2022-01-12 16:38:58 -0600

    Mejora en la apariencia de interfas de texto

--- facto.py
+++ facto.py
@@ -3,11 +3,19 @@ import pandas as pd
 import json
 from Modules import cargar_tarifas, verificar_archivo, Procesar_archivo
 from openpyxl import load_workbook
+import emoji
+
 
 def run():
+    class bcolors:
+        OK = '\033[92m' #GREEN
+        WARNING = '\033[93m' #YELLOW
+        FAIL = '\033[91m' #RED
+        RESET = '\033[0m' #RESET COLOR
+
     nombre=input("Introduce el nombre del archivo de consumos a procesar:  ")
     print('\n\n')
-    print("El archivo esta siendo procesado ...")
+    print(emoji.emojize(f":hourglass_not_done: {bcolors.WARNING}El archivo esta siendo procesado ...{bcolors.RESET}"))
     try: 
         archivo = verificar_archivo.abrir(nombre)
         periodo = archivo[1]
@@ -56,22 +64,31 @@ def run():
         writer.save()
 
         print('\n\n')
-        print("Archivo procesado correctamente")
-        print("Archivo guardado correctamente")
-        print("Terminado con normalidad")
+        print(emoji.emojize(f':check_mark_button: {bcolors.OK} Archivo procesado correctamente {bcolors.RESET} '))
+        print(emoji.emojize(f':check_mark_button: {bcolors.OK} Archivo guardado correctamente {bcolors.RESET}'))
+        print(emoji.emojize(f':check_mark_button: {bcolors.OK} Terminado con normalidad {bcolors.RESET}'))
+        print('\n\n')
+        
 
 
                    
     except Exception as e :
-        print("Algo ha salido mal al procesar el archivo...")
-        print(e)
+        print('\n\n')
+        print(emoji.emojize(f":cross_mark: {bcolors.FAIL}Algo ha salido mal al procesar el archivo...{bcolors.RESET}"))
+        print(emoji.emojize(f":cross_mark: {bcolors.FAIL}{e}: {nombre}{bcolors.RESET}"))
+        print('\n\n')
 
 
 if __name__ =='__main__':
+    class bcolors:
+        OK = '\033[92m' #GREEN
+        WARNING = '\033[93m' #YELLOW
+        FAIL = '\033[91m' #RED
+        RESET = '\033[0m' #RESET COLOR
     print('\n\n')
-    print("*******************************************************************************")
-    print("**                  Ejecutable de facturacion a corrugados                   **")
-    print("*******************************************************************************")
+    print(f"{bcolors.OK}*******************************************************************************{bcolors.RESET}")
+    print(f"{bcolors.OK}**                  Ejecutable de facturacion a corrugados                   **{bcolors.RESET}")
+    print(f"{bcolors.OK}*******************************************************************************{bcolors.RESET}")
     print('\n\n')
     run()
 
